0cae141601117cf0d5de9b9510c88e1d67885b87
andresro30 <andresrochag15@hotmail.com>
2020-04-09 12:07:32 -0500

    arreglando front conductor

---
+++ src/main/resources/static/webapp/Conductor/assets/js/apliclient.js
@@ -0,0 +1,46 @@
+var apiclient = ( function () {
+
+	/* Implementar API de conductor !! */ 
+	
+    var getPasajeros = (function (callback) {
+
+	
+        axios({
+            method: 'GET',
+            url: '/uniwheels/getPasajeros',
+
+        })
+            .then(response => callback(response.data))
+            .catch(error => console.log(error));
+    });
+	
+	var getSolicitudesPasajeros = (function (callback) {
+
+        axios({
+            method: 'GET',
+            url: '/uniwheels/getSolicitudesPasajeros',
+
+        })
+            .then(response => callback(response.data))
+            .catch(error => console.log(error));
+    });
+
+	/*
+    var agregarPasajero = (function (username) {
+
+        axios({
+            method: 'POST',
+            url: '/uniwheels/addPassanger/'+username,
+
+        })
+            .catch(error => console.log(error));
+    });
+	*/
+
+    return{
+        getPasajeros: getPasajeros,
+        getSolicitudesPasajeros: getSolicitudesPasajeros
+		/* agregarPasajero: agregarPasajero*/
+		
+    }
+})();
---
+++ src/main/resources/static/webapp/Conductor/assets/js/app.js
@@ -0,0 +1,53 @@
+var app = (function(){
+
+	
+	var getPasajeros = function(){
+		//var lista = apiclient.getConductoresDisponibles(addConductores);
+		
+	};
+	
+	
+	var addPasajeros = function(pasajeros){
+		$("#tablePasajeros > tbody").empty();
+		pasajeros.map(function(element){
+			/*
+			$("#pasajerosAceptados > tbody").append(
+				"<tr> <td>" +
+				element.conductorName +
+				"</td>" +
+				"<td>" +
+				element.tiempoRecorrido +
+				"</td> " +
+				"<td><form><button type='button' onclick='apiclient.agregarPosibleConductor("+"\""+element.conductorName+"&quot)' >Agregar</button></form></td>" +
+				"</tr>"
+			*/
+			);
+		});
+	};
+	
+	
+	var addSolicitudes = function(pasajeros){
+		$("#tableSolicitudes > tbody").empty();
+		pasajeros.map(function(element){
+			/*
+			$("solicitudesPasajeros > tbody").append(
+				"<tr> <td>" +
+				element.conductorName +
+				"</td>" +
+				"<td>" +
+				element.tiempoRecorrido +
+				"</td> " +
+				"<td><form><button type='button' onclick='apiclient.agregarPosibleConductor("+"\""+element.conductorName+"&quot)' >Agregar</button></form></td>" +
+				"</tr>"
+			*/
+			);
+		});
+	};
+	
+	return{	
+	    getPasajeros: getPasajeros,
+		addPasajeros: addPasajeros,
+		addSolicitudes: addSolicitudes
+	};
+	
+})();
