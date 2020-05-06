b76b8bc5e90f2ba8793c77d133a78f9521fff45e
andresro30 <andresrochag15@hotmail.com>
2020-05-05 22:36:51 -0500

    arreglo card pasajero

--- src/main/resources/static/webapp/Pasajero/assets/js/app.js
+++ src/main/resources/static/webapp/Pasajero/assets/js/app.js
@@ -67,37 +67,51 @@ var app = (function(){
 				var conductorData = JSON.parse(conductor.body);
 				console.log(conductorData);
 
-				var card = '<div class="card" style="width: 30rem; text-align: center">'+
-					'<img src="..." class="card-img-top" alt="...">'+
+				var card = '<div class="card" style="width: 30rem; text-align: center; background-color: #333333">'+
+					'<img src="../../images/pic01.jpg" class="card-img-top" alt="...">'+
 					'<div class="card-body">'+
 					'<h5 class="card-title">'+conductorData.conductorName+'</h5>'+
-					'<p class="card-text">El valor de la carrera es de: '+conductorData.ruta.precio+'.</p>'+
-					'<p class="card-text">Información de su carro:</p>'+
+					'<p class="card-text" style="color:#FFFFFF">El valor de la carrera es de: '+conductorData.ruta.precio+'.</p>'+
+					'<p class="card-text" style="color:#FFFFFF">Información de su carro:</p>'+
 					'</div>'+
-					'<ul class="list-group list-group-flush">'+
-					'<li class="list-group-item">'+conductorData.carro.marca+'</li>'+
-					'<li class="list-group-item">'+conductorData.carro.modelo+'</li>'+
-					'<li class="list-group-item">'+conductorData.carro.placa+'</li>'+
+					'<ul class="list-group list-group-flush" style=" background-color: #333333">'+
+					'<li class="list-group-item" style="color:#FFFFFF; background-color: #333333">'+conductorData.carro.marca+'</li>'+
+					'<li class="list-group-item" style="color:#FFFFFF; background-color: #333333">'+conductorData.carro.modelo+'</li>'+
+					'<li class="list-group-item" style="color:#FFFFFF; background-color: #333333">'+conductorData.carro.placa+'</li>'+
 					'</ul>'+
 					'<div class="card-body">'+
 					'<a href="#" class="card-link" onclick="alert("Metodo en construcción")">Cancelar viaje</a>'+
 					'</div>'+
 					'</div>';
-
-
-				var markup = '<div id="div-table" class="content" style="text-align: center;">'+
+					
+				var markup2 = '<div id="div-table">'+
 					'<br></br>'+
 					'<header>'+
-					'<h2>Mi Condutor</h2>'+
+						'<h2>Mi Condutor</h2>'+
 					'</header>'+
-					'<br></br>'+card+
-					'<br></br>';
-
-				$("#div-table").append(markup);
+					'<br></br>'+
+					'<div class="inner">'+
+						'<article>'+
+							'<div class="content">'+
+								card+
+							'</div>'+
+						'</article>'+
+						'<article class="alt">'+
+							'<div class="content">'+
+								'<div class="image fit">'+
+									//'<img src="/webapp/Pasajero/images/mapa.jpg" width="100%" height="80%" />'+
+								'</div>'+
+							'</div>'+
+						'</article>'+
+					'</div>'+
+					'</div>';
+			
+				$("#div-table").append(markup2);
 			});
 		});
 	}
-
+	
+	
 	var agregarPosiblePasajero = function(conductorName){
 		console.info('Connecting to WS...');
 		var socket = new SockJS('/stompendpoint');
@@ -114,7 +128,6 @@ var app = (function(){
 		get:get,
 		infoViaje: infoViaje,
 		pasajeroAceptado:pasajeroAceptado
-
 	};
 	
 })();
